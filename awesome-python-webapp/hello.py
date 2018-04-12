df58eb36c951d09b1ec0f163c62dd858fa221db7
smelllu <31501685+smelllu@users.noreply.github.com>
2018-04-12 14:40:23 +0800

    Add files via upload

---
+++ awesome-python-webapp/hello.py
@@ -0,0 +1,32 @@
+# print 'jkdjakgjk'
+
+# print abs(1)
+
+# print int(12.33333)
+
+# x = 10
+
+# def my_abs(x):
+# 	if not isinstance(x,(int,float)):
+# 		raise TypeError('bad operand type')
+# 		pass
+# 	if x >= 0:
+#         return x
+#     else:
+#         return -x
+# print x
+
+# def nop():
+# 	pass
+
+def power(x):
+	return x*x
+
+print power(2)
+
+def power2(x,n)
+	s = 1
+	while n > 0:
+		n = n - 1
+		s = s * x
+	return s
